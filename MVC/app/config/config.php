ded2340d9a8a8ca9c4927358cd786351aa6d6a46
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-03-25 12:45:47 +0200

    Create config.php

---
+++ MVC/app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'miublog');
+
+// App Root
+//echo dirname(dirname(__FILE__));
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/mvc/public/');
+// Site Name
+define('SITENAME', 'MIU Blog MVC');
+
+define('APP_VERSION', '1.2');
+
+//public pages path
+define('VIEWS_PATH', '../app/views/');

5a371eac6c3a9a1985675f5f784955cffc021811
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-03-25 12:46:54 +0200

    Create header.php

---
+++ MVC/app/views/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="undefined" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
+  <title><?php echo SITENAME; ?></title>
+</head>
+
+<body>
+  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+  <div class="container">

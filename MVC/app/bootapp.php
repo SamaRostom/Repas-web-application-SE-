72cae97916b70f7dbbff0d4049cb39d7fad97121
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-03-25 12:45:44 +0200

    Create bootapp.php

---
+++ MVC/app/bootapp.php
@@ -0,0 +1,20 @@
+<?php
+// Load Config
+require_once 'config/config.php';
+
+// Load Helpers
+require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
+
+// Load Core Libraries
+require_once 'libraries/Model.php';
+require_once 'libraries/View.php';
+require_once 'libraries/Controller.php';
+
+require_once 'libraries/Core.php';
+require_once 'libraries/Database.php';
+
+//Autoload Core Libraries
+//spl_autoload_register(function ($className) {
+ //   require_once 'libraries/' . $className . '.php';
+//});

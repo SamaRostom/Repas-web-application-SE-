916df99ef5336fb036f920baeb34c21275db2765
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-03-31 01:10:02 +0200

    Update RegisterModel.php

--- MVC/app/models/RegisterModel.php
+++ MVC/app/models/RegisterModel.php
@@ -2,67 +2,144 @@
 require_once 'UserModel.php';
 class RegisterModel extends UserModel
 {
-    public  $title = 'User Registration Page';
-    protected $name;
-    protected $nameErr;
-    protected $confirmPassword;
-    protected $confirmPasswordErr;
-
+    public  $title = 'Registration Page';
+    protected $Username;
+    protected $UsernameErr;
+    protected $Password;
+    protected $PasswordErr;
+    protected $Address;
+    protected $AddressErr;
+    protected $Phone_Number;
+    protected $Phone_NumberErr;
+    protected $Backup_Number;
+    protected $Backup_NumberErr;
 
     public function __construct()
     {
         parent::__construct();
-        $this->name     = "";
-        $this->nameErr = "";
+        $this->Username     = "";
+        $this->UsernameErr = "";
 
-        $this->confirmPassword = "";
-        $this->confirmPasswordErr = "";
-    }
+        $this->Password = "";
+        $this->PasswordErr = "";
 
-    public function getName()
-    {
-        return $this->name;
-    }
+        $this->Address = "";
+        $this->AddressErr = "";
 
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+        $this->Phone_Number = "";
+        $this->Phone_NumberErr = "";
 
-    public function getNameErr()
-    {
-        return $this->nameErr;
+        $this->Backup_Number = "";
+        $this->Backup_NumberErr = "";
     }
 
-    public function setNameErr($nameErr)
+    public function getUsername()
     {
-        $this->nameErr = $nameErr;
+        return $this->Username;
     }
 
-    public function getConfirmPassword()
+    public function setUsername($Username)
     {
-        return $this->confirmPassword;
-    }
-    public function setConfirmPassword($confirmPassword)
-    {
-        $this->confirmPassword = $confirmPassword;
+        $this->Username = $Username;
     }
 
-    public function getConfirmPasswordErr()
+    public function getUsernameErr()
     {
-        return $this->confirmPasswordErr;
+        return $this->UsernameErr;
     }
-    public function setConfirmPasswordErr($confirmPasswordErr)
+
+    public function setUsernameErr($UsernameErr)
     {
-        $this->confirmPasswordErr = $confirmPasswordErr;
+        $this->UsernameErr = $UsernameErr;
     }
+
+    public function getPassword()
+    {
+        return $this->Password;
+    }
+    public function setPassword($Password)
+    {
+        $this->Password = $Password;
+    }
+
+    public function getPasswordErr()
+    {
+        return $this->PasswordErr;
+    }
+    public function setPasswordErr($PasswordErr)
+    {
+        $this->PasswordErr = $PasswordErr;
+    }
+
+    public function getAddress()
+    {
+        return $this->Address;
+    }
+
+    public function setAddress($Address)
+    {
+        $this->Address = $Address;
+    }
+
+    public function getAddressErr()
+    {
+        return $this->AddressErr;
+    }
+
+    public function setAddressErr($AddressErr)
+    {
+        $this->Address = $AddressErr;
+    }
+
+    public function getPhone_Number()
+    {
+        return $this->Phone_Number;
+    }
+
+    public function setPhone_Number($Phone_Number)
+    {
+        $this->Phone_Number = $Phone_Number;
+    }
+
+    public function getPhone_NumberErr()
+    {
+        return $this->Phone_NumberErr;
+    }
+
+    public function setPhone_NumberErr($Phone_NumberErr)
+    {
+        $this->Phone_NumberErr = $Phone_NumberErr;
+    }
+
+    public function getBackup_Number()
+    {
+        return $this->Backup_Number;
+    }
+
+    public function setBackup_Number($Backup_Number)
+    {
+        $this->Backup_Number = $Backup_Number;
+    }
+
+    public function getBackup_NumberErr()
+    {
+        return $this->Backup_NumberErr;
+    }
+
+    public function setBackup_NumberErr($Backup_NumberErr)
+    {
+        $this->Backup_NumberErr = $Backup_NumberErr;
+    }
+
 
     public function signup()
     {
-        $this->dbh->query("INSERT INTO users (`name`, `email`, `password`) VALUES(:uname, :email, :pass)");
-        $this->dbh->bind(':uname', $this->name);
-        $this->dbh->bind(':email', $this->email);
-        $this->dbh->bind(':pass', $this->password);
+        $this->dbh->query("INSERT INTO person (`Username`, `Password`, `Address`, `Phone_Number`, `Backup_Number`) VALUES(':Username', ':Password', ':Address', ':Phone_Number', ':Backup_Number')");
+        $this->dbh->bind(':Username', $this->Username);
+        $this->dbh->bind(':Password', $this->Password);
+        $this->dbh->bind(':Address', $this->Address);
+        $this->dbh->bind(':Phone_Number', $this->Phone_Number);
+        $this->dbh->bind(':Backup_Number', $this->Backup_Number);
 
         return $this->dbh->execute();
     }

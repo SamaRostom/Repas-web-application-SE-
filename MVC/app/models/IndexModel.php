f7cf7ff2e677bad16ca4514620e8d98fa4612ab8
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-04-01 00:27:19 +0200

    Update IndexModel.php

--- MVC/app/models/IndexModel.php
+++ MVC/app/models/IndexModel.php
@@ -1,6 +1,6 @@
 <?php
 class IndexModel extends model
 {
-     public $title = 'MIU SE305 Blog ' . APP_VERSION;
+     public $title = 'Repas ' . APP_VERSION;
      public $subtitle = 'Example of MVC PHP framework for SE305';
 }

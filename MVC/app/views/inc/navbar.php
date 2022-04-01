19e3e19784d0935f13a415b5da120c4bdb6720c8
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-04-01 03:44:13 +0200

    Update navbar.php

--- MVC/app/views/inc/navbar.php
+++ MVC/app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo URLROOT . 'public'; ?>"><?php echo SITENAME; ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

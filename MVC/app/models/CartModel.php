9053ec04874c57e7128be4bb115208c6323469a1
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-04-04 01:18:01 +0200

    Create CartModel.php

---
+++ MVC/app/models/CartModel.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'UserModel.php';
+class CartModel extends UserModel
+{
+    public  $title = 'Cart Page';
+    protected $Meal_ID;
+    protected $Meal_Name;
+    protected $Meal_Price;
+    
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->Meal_ID     = "";
+        $this->Meal_Name = "";
+        $this->Meal_Price = "";
+    }
+
+    public function getMeal_ID()
+    {
+        return $this->Meal_ID;
+    }
+
+    public function setMeal_ID($Meal_ID)
+    {
+        $this->Meal_ID = $Meal_ID;
+    }
+
+    public function getMeal_Name()
+    {
+        return $this->Meal_Name;
+    }
+    public function setMeal_Name($Meal_Name)
+    {
+        $this->Meal_Name = $Meal_Name;
+    }
+
+    public function getMeal_Price()
+    {
+        return $this->Meal_Price;
+    }
+
+    public function setMeal_Price($Meal_Price)
+    {
+        $this->Meal_Price = $Meal_Price;
+    }
+
+    public function addtocart()
+    {
+        // $this->dbh->query("INSERT INTO orders (`Username`, `Password`, `Address`, `Phone_Number`, `Backup_Number`) VALUES(':Username', ':Password', ':Address', ':Phone_Number', ':Backup_Number')");
+        // $this->dbh->bind(':Username', $this->Username);
+        // $this->dbh->bind(':Password', $this->Password);
+        // $this->dbh->bind(':Address', $this->Address);
+
+        // return $this->dbh->execute();
+    }
+}

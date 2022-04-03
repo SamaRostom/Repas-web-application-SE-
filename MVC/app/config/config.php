caa3e3207376c1abc70d71df7070e0d579ff7c57
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-04-03 16:19:43 +0200

    Update config.php

--- MVC/app/config/config.php
+++ MVC/app/config/config.php
@@ -8,8 +8,8 @@ define('DB_NAME', 'repas');
 // App Root
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+// URL Root 
+define('URLROOT', 'http://localhost/software/repas-web-application--se-/mvc/public/');
 // Site Name
 define('SITENAME', 'MIU Blog MVC');
 
@@ -17,3 +17,6 @@ define('APP_VERSION', '1.2');
 
 //public pages path
 define('VIEWS_PATH', '../app/views/');
+
+// img Root 
+define('IMGROOT', 'http://localhost/software/repas-web-application--se-/mvc/public/images/');

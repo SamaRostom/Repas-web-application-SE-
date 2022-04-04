ef63cbbe174e035ded2e3c319a6c5d91e286ceb1
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-04-04 22:51:40 +0200

    Update appetizers.php

--- MVC/app/views/users/appetizers.php
+++ MVC/app/views/users/appetizers.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="style/appetizers.css">
 </head>
 
 <body>

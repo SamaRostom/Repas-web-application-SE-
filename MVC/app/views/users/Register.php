5b178731e6d6bac575b4020a6d1cf4fd53d5474d
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-04-04 15:19:27 +0200

    Update Register.php

--- MVC/app/views/users/Register.php
+++ MVC/app/views/users/Register.php
@@ -5,7 +5,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>       
    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  
+   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/Login.css">
 
 <script>
   function ShowPassword() {

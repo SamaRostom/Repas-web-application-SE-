2d2d66aa0a59ae453377be2a8032e015077693c6
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-04-04 16:56:44 +0200

    Update ContactModel.php

--- MVC/app/models/ContactModel.php
+++ MVC/app/models/ContactModel.php
@@ -3,7 +3,7 @@ class ContactModel extends model{
 	public $title ='Contact Page';
 	public $Phone=" Phone Number: +20 1277357337";
 	public $Email=" Email: repasfrozen@gmail.com";
-	public $Address="";
+	public $Address="Address: Tagamoa elkhames, New Cairo";
 }
 
 ?>

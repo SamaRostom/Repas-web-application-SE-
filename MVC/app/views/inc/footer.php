e8359fdb24d460e684241e521fc471c33ca3ff46
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-04-05 03:37:57 +0200

    Update footer.php

--- MVC/app/views/inc/footer.php
+++ MVC/app/views/inc/footer.php
@@ -1,7 +1,17 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="<?php echo URLROOT; ?>js/main.js"></script>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/footer.css">
   </body>
   
-
+  <div class='networks fs-2 text-center'>
+    <a href='https://www.facebook.com/Repasfrozenmeals' target='blank'><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+    <a href='https://www.instagram.com/repas_frozenmeals/' target='blank'><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    </div>
+   
+         <a href="<?php echo URLROOT . 'pages/about'; ?>" class="menu">About Us</a>
+    <a href="<?php echo URLROOT . 'pages/contact'; ?>" class="menu">Contact Us</a> 
   </html>
+ 
+    

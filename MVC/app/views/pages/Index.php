7aa9705409f0f40697988fd42b01c0d0b7898783
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-05-25 15:34:50 +0200

    Update Index.php

--- MVC/app/views/pages/Index.php
+++ MVC/app/views/pages/Index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <title>Home</title>
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     
@@ -33,6 +34,7 @@
 </head>
 <body>
 <?php
+// require IMGROOT;
 class Index extends View
 {
   public function output()

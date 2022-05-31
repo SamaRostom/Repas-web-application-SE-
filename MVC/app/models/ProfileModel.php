da289f35f6ee0ae6f53324867e10b34dcfdbfdf9
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-05-31 09:06:54 +0200

    Update ProfileModel.php

--- MVC/app/models/ProfileModel.php
+++ MVC/app/models/ProfileModel.php
@@ -3,11 +3,55 @@ require_once 'UserModel.php';
 class ProfileModel extends UserModel
 {
     public  $title = 'Profile Page';
+    public $ID_Person,$Username,$ID_Type,$Address;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ID_Person    = '';
+        $this->Username = '';
+        $this->ID_Type    = '';
+        $this->Address    = '';
+    }
+
+    public function getID_Person()
+    {
+        return $this->ID_Person;
+    }
+    public function setID_Person($ID_Person)
+    {
+        $this->ID_Person = $ID_Person;
+    }
+
+    public function getUsername()
+    {
+        return $this->Username;
+    }
+    public function setUsername($Username)
+    {
+        $this->Username = $Username;
+    }
+    public function getID_Type()
+    {
+        return $this->ID_Type;
+    }
+    public function setID_Type($ID_Type)
+    {
+        $this->ID_Type = $ID_Type;
+    }
+    public function getAddress()
+    {
+        return $this->Address;
+    }
+    public function setAddress($Address)
+    {
+        $this->Address = $Address;
+    }
 
     public function ViewProfile()
     {
         $this->dbh->query('SELECT * from person WHERE ID_Person=:ID_Person');
-        $this->dbh->bind(':ID_Person', $this->idperson);
+        $this->dbh->bind(':ID_Person', $this->ID_Person);
          
         $record = $this->dbh->resultSet();
 

0d39d8eab0becdf2cf636e8b4556f9abd86b6ef8
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-06-01 11:52:40 +0200

    Update header.php

--- MVC/app/views/inc/header.php
+++ MVC/app/views/inc/header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="undefined" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
   <link href="<?php echo URLROOT; ?>js/main.js">

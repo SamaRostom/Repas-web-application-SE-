d2431219d3abc1b53a50febdaf94e3d3167c1584
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-06-01 11:30:27 +0200

    Update Profile.php

--- MVC/app/views/users/Profile.php
+++ MVC/app/views/users/Profile.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<title>Profile</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>    
-  	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="style/Profile.css">
 
 	<script>
@@ -29,7 +30,8 @@ class Profile extends View
 	$edaction = URLROOT . 'users/editProfile';
 	$usn = $this->model->ViewProfile();
 	$Ent = $this->model->ID_Type();
-	$username = $usn->address;
+	//$username = $usn->address;
+	$e=$_SESSION["ID_Person"];
 		
     // $username = $this->getModel()->username;
 
@@ -37,7 +39,7 @@ class Profile extends View
     // $user_name = $_SESSION['user_name'];
 
     require APPROOT . '/views/inc/header.php';
-    $text = <<<EOT
+    echo $e;?>	
 <div class='container my-5 row col-10 col-sm-8 col-lg-7 m-auto rounded py-5 shadow'>
 	<div class="imgContainer m-auto col-4">
 		
@@ -48,16 +50,16 @@ class Profile extends View
 		</div>
 	</div>
 
+
 	<div class="userDetails col-10 col-lg-6 m-auto offset-lg-1">
 
-	<i class="fas fa-user"></i><?php echo $username;?><br><hr>
-	<i class="fas fa-map-marker-alt"></i><?php echo $Ent;?><br><hr>
+	<i class="fas fa-user"></i><?php echo $e;?><br><hr>
+	<i class="fas fa-map-marker-alt"></i><?php echo $e;?><br><hr>
 	<i class="fas fa-phone-alt"></i>"Phone_Number"-<br> "Backup_Number"<br><hr>
 	    
 	</div>	
 </div>	
-EOT;
-    echo $text;
+<?php
     require APPROOT . '/views/inc/footer.php';
   }
   public function o()

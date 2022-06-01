f0964d3d556e8a33219f78f68bd70b0b31ef36a0
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-01 17:17:13 +0200

    Update footer.php

--- MVC/app/views/inc/footer.php
+++ MVC/app/views/inc/footer.php
@@ -1,17 +1,25 @@
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="<?php echo URLROOT; ?>js/main.js"></script>
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/footer.css">
-  </body>
-  
-  <div class='networks fs-2 text-center'>
-    <a href='https://www.facebook.com/Repasfrozenmeals' target='blank'><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-    <a href='https://www.instagram.com/repas_frozenmeals/' target='blank'><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        .networks a,
+        .links p, .links a{
+            text-decoration: none;
+            color: rgb(70, 110, 170);
+            padding: 0rem .5rem
+        }
+    </style>
+</head>
+<body>
+  <div class='py-4 bg-light col-12 footer'>
+    <div class='networks fs-4 text-center'>
+    <a href='https://www.facebook.com/Repasfrozenmeals' target='blank'><i class="fab fa-facebook"></i></a>
+    <a href='https://www.instagram.com/repas_frozenmeals/' target='blank'><i class="fab fa-instagram"></i></a>
     </div>
-   
-         <a href="<?php echo URLROOT . 'pages/about'; ?>" class="menu">About Us</a>
-    <a href="<?php echo URLROOT . 'pages/contact'; ?>" class="menu">Contact Us</a> 
-  </html>
- 
-    
+    <div class='links text-center'>
+        <p class='my-2'><a href="<?php echo URLROOT . 'pages/about'; ?> " class="menu">About Us</a>  &#9679; <a href="<?php echo URLROOT . 'pages/contact'; ?>" class="menu">Contact Us</a>  &#9679; <a href=<?php echo URLROOT . 'users/Register'?>> Join Us</a></p>
+    </div>
+    </div>
+</body>
+</html>

7e72929dab3cfebbc609fe78e5004a23770226dd
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-06-02 19:25:18 +0200

    Update MealsDetailsModel.php

--- MVC/app/models/MealsDetailsModel.php
+++ MVC/app/models/MealsDetailsModel.php
@@ -4,13 +4,14 @@ class MealsDetailsModel extends UserModel
 {
     public  $title = 'MealsDetails Page';
 
-    protected $Meal_ID,$Meal_Name,$Meal_Price;
+    protected $Meal_ID,$Meal_Name,$Meal_Price,$Quantity;
 
     public function __construct()
     {
         parent::__construct();
         $this->Meal_ID    = '';
         $this->Meal_Name = '';
+        $this->Quantity    = 0;
         $this->Meal_Price    = '';
     }
 
@@ -30,6 +31,19 @@ class MealsDetailsModel extends UserModel
     {
         $this->Meal_Name = $Meal_Name;
     }
+    public function getQuantity()
+    {
+        return $this->Quantity;
+        return $Quantity+=1;
+    }
+    public function Quantity()
+    {
+        $this->Quantity+=1;
+    }
+    public function setQuantity($Quantity)
+    {
+        $this->Quantity = $Quantity;
+    }
     public function getMeal_Price()
     {
         return $this->Meal_Price;
@@ -46,10 +60,14 @@ class MealsDetailsModel extends UserModel
          
         $record = $this->dbh->resultSet();
         foreach($record as $x){
-            $this->Meal_ID=$x->Meal_ID;
-            $this->Meal_Name=$x->Meal_Name;
-            $this->Meal_Price=$x->Meal_Price;
+            // $this->Meal_ID=$x->Meal_ID;
+            // $this->Meal_Name=$x->Meal_Name;
+            // $this->Meal_Price=$x->Meal_Price;
+            $_SESSION["Meal_ID"]=$x->Meal_ID; 
+            $_SESSION["Meal_Name"]=$x->Meal_Name;
+            $_SESSION["Meal_Price"]=$x->Meal_Price;  
         }
+        $_SESSION["Quantity"]=$this->Quantity;
         return $record;
 
     }

b67f8db3dbf6c33a68675b9bd6853c644769e392
YaraLotfy <yara1903165@miuegypt.edu.eg>
2022-06-03 02:32:25 +0200

    Update Index.php

--- MVC/app/views/pages/Index.php
+++ MVC/app/views/pages/Index.php
@@ -7,9 +7,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     
-	<link rel="stylesheet" href="<?php echo URLROOT; ?>css/index.css">
+<link rel="stylesheet" href="<?php echo URLROOT.'css/index.css'?>">
 
-	<script>
+
+
+	<!-- <script>
  function currentDiv(n) {
   showDivs(slideIndex = n);
  }
@@ -29,8 +31,127 @@
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-opacity-off";
 }
+</script> -->
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 </script>
-  
+<style>
+/* * {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}  */
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 2500px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.9s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.9s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 60s;
+}
+
+@keyframes fade {
+  from {opacity: .9} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+</style>
 </head>
 <body>
 <?php
@@ -48,56 +169,39 @@ class Index extends View
     $text = <<<EOT
 	
 
-	<div class="w3-content" style="max-width:1200px">
-	<img class="mySlides" src="images/g.jpeg" style="width:100%;display:none">
-	<img class="mySlides" src="images/d.jpeg" style="width:100%">
-	<img class="mySlides" src="images/f.jpeg" style="width:100%;display:none">
-	
-  
-	<div class="w3-row-padding w3-section">
-	  <div class="w3-col s4">
-		<img class="demo w3-opacity w3-hover-opacity-off" src="images/g.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(1)">
-	  </div>
-	  <div class="w3-col s4">
-		<img class="demo w3-opacity w3-hover-opacity-off" src="images/d.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(2)">
-	  </div>
-	  <div class="w3-col s4">
-		<img class="demo w3-opacity w3-hover-opacity-off" src="images/f.jpeg" style="width:100%;cursor:pointer" onclick="currentDiv(3)">
-	  </div>
-	</div>
-  </div>
 
-  <h3> Categories</h3>
-  <section class="cards">
-<div>
-	<div class="column">
-		<div class="card" id="equipcard">
-			<div class="image1">
-				<img src="images/h.jpeg" width=100% height=50% class="card-img-top">
-				<h3>Cooked</h3>
-			</div>
-		</div>
-	</div>
 
-	<div class="column">
-	<div class="card" id="equipcard">
-		<div class="image2">
-			<img src="images/m.jpeg" width=100%  height=20% class="card-img-top">
-			<h3>Frozen</h3>
-		</div>
+<div class="slideshow-container">
 
-	</div>
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="images/e.jpeg" style="width:100%">
+  <div class="text">Caption Text</div>
 </div>
-<div class="column">
-	<div class="card" id="equipcard">
-		<div class="image2">
-			<img src="images/e.jpeg" width=100%  height=50% class="card-img-top">
-			<h3>Meals and mini sandwiches</h3>
-		</div>
 
-	</div>
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="images/m.jpeg" style="width:100%">
+  <div class="text">Caption Two</div>
 </div>
-</section>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="images/h.jpeg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
 
 <h2>   To all the housewives you deserve all the best......</h2>
   

a10228b7feca455e7688705ae4bb35643498024b
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-06-03 18:56:59 +0200

    Create DashboardModel.php

---
+++ MVC/app/models/DashboardModel.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'UserModel.php';
+class DashboardModel extends UserModel
+{
+    public  $title = 'Dashboard Page';
+
+    public function dashboard()
+    {
+        
+    }
+
+    public function TotalNumberofUsers()
+    {
+        $this->dbh->query('SELECT ID_Person FROM person WHERE ID_Type=2');
+         
+        $record = $this->dbh->execute();
+        $TotalNumberofUsers = $this->dbh->rowCount();
+        $_SESSION['tnu']=$TotalNumberofUsers;
+        return $TotalNumberofUsers;
+        
+        
+        // $conn = new mysqli("localhost", "root", "", "repas");
+        // $ss = "SELECT COUNT(ID_Person) AS TotalNumberofHikers FROM person WHERE ID_Type=2"  ;
+        // $rr = mysqli_query ($conn ,$ss);
+        // $dd =  mysqli_fetch_assoc($rr);
+        // $TotalNumberofHikers=$dd['TotalNumberofHikers'];
+        // $_SESSION['tnu']=$TotalNumberofHikers;
+        // return $TotalNumberofHikers;
+    }
+
+    public function TotalNumberofMeals()
+    {
+        $this->dbh->query('SELECT Meal_Id FROM meals');
+         
+        $record = $this->dbh->execute();
+        $tnm = $this->dbh->rowCount();
+        $_SESSION['tnm']=$tnm;
+        return $tnm;
+    }
+
+    public function TotalNumberofOrders()
+    {
+        $this->dbh->query('SELECT Order_ID FROM orders');
+         
+        $record = $this->dbh->execute();
+        $tno = $this->dbh->rowCount();
+        $_SESSION['tno']=$tno;
+        return $tno;
+    }
+    
+    public function TotalRevenue()
+    {
+        $this->dbh->query('SELECT * FROM orders');
+        $tr=0; 
+        $record = $this->dbh->resultSet();
+        foreach($record as $x){
+            $tr+=$x->Total_Price;
+        }
+        return $tr;
+    }
+
+    public function TopMeal()
+    {
+        $this->dbh->query('SELECT M.Meal_ID, M.Description, M.Meal_Name, M.Meal_Price, M.Meal_Image, M.ID_Category, COUNT(O.Meal_ID) as MealCount FROM `order_details` as O INNER JOIN  `meals` as M ON O.Meal_ID = M.Meal_ID GROUP BY M.Meal_ID ORDER BY MealCount DESC LIMIT 1');
+        $record = $this->dbh->resultSet();
+
+        return $record;
+    }
+
+    public function usersc(){
+        $this->dbh->query('SELECT * FROM person WHERE ID_Type=2');
+        $record = $this->dbh->resultSet();
+
+        return $record;
+       
+    }
+
+    public function adminsc(){
+        $this->dbh->query('SELECT * FROM person WHERE ID_Type=1');
+        $record = $this->dbh->resultSet();
+
+        return $record;
+       
+    }
+    
+    public function ordersc(){
+        $this->dbh->query('SELECT * FROM orders');
+        $record = $this->dbh->resultSet();
+
+        return $record;
+       
+    }
+
+    // public function deleteadmin($ID){
+    //     $this->dbh->query('DELETE FROM `person` WHERE ID_Person = :ID_Person');
+    //     $this->dbh->bind(':ID_Person', $ID);
+    //     $record = $this->dbh->execute();
+
+    //     return $record;
+       
+    // }
+}

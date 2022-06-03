26cfe5c4c3bc93649bed867456386b6f10d3f648
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-03 19:14:27 +0200

    Update contact.php

--- MVC/app/views/pages/contact.php
+++ MVC/app/views/pages/contact.php
@@ -4,6 +4,10 @@
 <title>Contact Us</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+
 <link rel="stylesheet" href="<?php echo URLROOT; ?>css/ContactUs.css">
 </head>
 <body>

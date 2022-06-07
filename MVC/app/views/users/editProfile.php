b28c7b84ab045635ca1d60e703087256310998d4
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-07 19:47:38 +0200

    Update editProfile.php

--- MVC/app/views/users/editProfile.php
+++ MVC/app/views/users/editProfile.php
@@ -6,22 +6,14 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>       
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link rel="stylesheet" href="<?php echo URLROOT . "/css/editProfile.css"?>">
-  <script>
-    function ShowPassword() {
-      var pass = document.querySelector('input[name="Password"]');
-      var icon = document.querySelector('.showBtn i');
-      if (pass.type === "password") {
-        pass.type = "text";
-        icon.setAttribute('class','far fa-eye-slash');
-      } else {
-        pass.type = "password";
-        icon.setAttribute('class','far fa-eye');
-      }
-    }
-  </script>
+ 
   <style>
       body{
         background-image: url("<?php echo IMGROOT."loginbackground.jpg";?>");
+        background-size: contain;
+	      background-repeat: no-repeat;	
+ 	      background-position-y: bottom;
+	      height:110vh;
       }
   </style>
 
@@ -39,20 +31,21 @@ class editProfile extends View
     require APPROOT . '/views/inc/header.php';
 	?>
 	<div class='col-10 col-md-8 col-lg-6 container p-4 my-5 mx-auto'>
+
 		<h1 class='display-6 mb-3 text-center'>Edit your Profile</h1>
 
-		Username: <input type= 'text' class='form-control' name= 'Username'  value='<?php echo $name; ?>'>
+    <form method="POST" action="">
 
-		<div class="input-group mb-3">
-        Password: <input type= 'password' class= 'form-control' name='Password' value="<?php echo $p; ?>">
-        <div class='showBtn' onclick="ShowPassword()"><i class='far fa-eye'></i></div>
-        </div>
-        Address: <input type= 'text'  class='form-control' name= 'Address' value="<?php echo $e; ?>">
+		Username: <input type= 'text' class='form-control' name= 'Username'  value='<?php echo $_SESSION['Username']; ?>'>
 
-		Phone Number: <input type= 'text'  class='form-control' name= 'Phone_Number' value="<?php echo $pn; ?>">
+    Password: <input type= 'password' class= 'form-control' name='Password' value="<?php echo $_SESSION['Password']; ?>">
 
-		Backup Number: <input type= 'text'  class='form-control' name= 'Backup Number' value="<?php echo $bn; ?>">
-		<div style='text-align:center'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Save'>
+    Address: <input type= 'text'  class='form-control' name= 'Address' value="<?php echo $_SESSION['Address']; ?>">
+
+		Phone Number: <input type= 'text'  class='form-control' name= 'Phone_Number' value="<?php echo $_SESSION['Phone_Number']; ?>">
+
+		Backup Number: <input type= 'text'  class='form-control' name= 'Backup_Number' value="<?php echo $_SESSION['Backup_Number']; ?>">
+		<div style='text-align:center'><input type='submit' class='btn btn-primary px-5 mt-4' name='Edit' value='Edit'>
 		<input type='button' class='btn btn-outline-dark px-5 ms-3 mt-4' onclick='history.back();' value='Cancel'></div>
 		</form>
 		</div>

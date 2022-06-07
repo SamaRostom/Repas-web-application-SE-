4e5d6600d1c693610f50416c47dc92d45ec5025f
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-07 17:21:37 +0200

    Update AddMeal.php

--- MVC/app/views/users/AddMeal.php
+++ MVC/app/views/users/AddMeal.php
@@ -3,7 +3,7 @@
 <head>
   <title></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" /><link rel="stylesheet" href="<?php echo URLROOT; ?>css/Login.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 </head>
 <body>
@@ -27,7 +27,7 @@ class AddMeal extends view
     <h1 class='display-6 text-center'>Add Meals</h1>
     <form action="" method="post" enctype='multipart/form-data'>
     <div class="input-group mt-4 mb-3">
-      <span class="input-group-text"><i class="fa-solid fa-plate-utensils"></i></span>
+      <span class="input-group-text"><i class="fa fa-list"></i></span>
       <input type="text" class="form-control" placeholder="Insert meal name" name="Meal_Name">
     </div>
 

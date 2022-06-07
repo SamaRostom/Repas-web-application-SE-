5a0594f9f3f96c31814108638c102d3f497cd806
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-07 13:08:03 +0200

    Update AddMeal.php

--- MVC/app/views/users/AddMeal.php
+++ MVC/app/views/users/AddMeal.php
@@ -3,8 +3,7 @@
 <head>
   <title></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/Login.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" /><link rel="stylesheet" href="<?php echo URLROOT; ?>css/Login.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 </head>
 <body>
@@ -23,12 +22,12 @@ class AddMeal extends view
   {
     $action = URLROOT . 'users/addmeal';
     $Url = URLROOT . 'users/Meals';
-    ?>
+    $text = <<<EOT
     <div class='col-10 col-md-7 col-lg-6 col-xl-5 m-auto form-container px-5 py-4 my-5'>
     <h1 class='display-6 text-center'>Add Meals</h1>
     <form action="$action" method="post" enctype='multipart/form-data'>
     <div class="input-group mt-4 mb-3">
-      <span class="input-group-text"><i class="fa fa-list"></i></span>
+      <span class="input-group-text"><i class="fa-solid fa-plate-utensils"></i></span>
       <input type="text" class="form-control" placeholder="Insert meal name" name="Meal_Name">
     </div>
 
@@ -56,7 +55,8 @@ class AddMeal extends view
     </div>
     </form> 
     </div>
-    <?php
+EOT;
+    echo $text;
   }
 }
 ?>

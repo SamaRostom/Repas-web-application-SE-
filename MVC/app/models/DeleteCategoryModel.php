eebd04b3e93891bb8ca6bbc04def971a7f3b83e9
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2022-06-07 15:30:47 +0200

    Update DeleteCategoryModel.php

--- MVC/app/models/DeleteCategoryModel.php
+++ MVC/app/models/DeleteCategoryModel.php
@@ -5,11 +5,15 @@ class DeleteCategoryModel extends UserModel
     public  $title = 'Delete Category Page';
     public function delete($ID)
     {
-        $this->dbh->query('DELETE FROM `meals_category` WHERE ID_Category = :ID_Category');
+        // $this->dbh->query('DELETE FROM `meals_category`,`meals` WHERE ID_Category = :ID_Category');
+
+        $this->dbh->query('DELETE FROM `meals` WHERE ID_Category = :ID_Category');
         $this->dbh->bind(':ID_Category', $ID);
         $record = $this->dbh->execute();
-
-        return $record;
+        // return $record;
+        $this->dbh->query('DELETE FROM `meals_category` WHERE ID_Category = :ID_Category');
+        $this->dbh->bind(':ID_Category', $ID);
+        $record2 = $this->dbh->execute();
     }
 }
 ?>

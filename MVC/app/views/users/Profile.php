0c5165c1a0bbbadbf6f8358b668c47d1f9ef28db
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-06-07 21:02:16 +0200

    Update Profile.php

--- MVC/app/views/users/Profile.php
+++ MVC/app/views/users/Profile.php
@@ -2,24 +2,15 @@
 <html>
 <head>
 	<title>Profile</title>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
 	<link rel="stylesheet" href="style/Profile.css">
-
-	<script>
-		function setImage(){
-		var img = document.querySelector('.imgContainer img')
-
-		if(img.src.endsWith('/')){
-			img.src = 'images/avatar.png'
-		}
-	}
-	</script>
 		
 </head>
-<body onload="setImage()">
+<body>
 	<?php
 class Profile extends View
 {
@@ -44,12 +35,12 @@ class Profile extends View
     require APPROOT . '/views/inc/header.php';
     ?>	
 <div class='container my-5 row col-10 col-sm-8 col-lg-7 m-auto rounded py-5 shadow'>
-	<div class="imgContainer m-auto col-4">
+	<div class="edit m-auto col-4">
 		
 		<div class="editIcon">
-			<a href="<?php echo $edaction; ?>">
-				<i class="fas fa-pen"></i>
-			</a>
+		 <form action="<?php echo $edaction; ?>">
+           <button type="submit" class="btn btn-dark px-3"> Edit <i class="fas fa-edit"></i></button>
+         </form>
 		</div>
 	</div>
 
@@ -58,7 +49,8 @@ class Profile extends View
 
 	<i class="fas fa-user"></i> <?php echo $name;?><br><hr>
 	<i class="fas fa-map-marker-alt"></i> <?php echo $e;?><br><hr>
-	<i class="fas fa-phone-alt"></i> <?php echo $pn;?><br>  <?php echo $bn;?><br><hr>
+	<i class="fas fa-phone-alt"></i> <?php echo $pn;?><br><hr>
+	<i class="fas fa-phone-alt"></i> <?php echo $bn;?><br>
 	    
 	</div>	
 </div>	
@@ -117,8 +109,7 @@ class Profile extends View
 
 	<i class="fas fa-user"></i><"ahahah"><br><hr>
 	<i class="fas fa-map-marker-alt"></i>"Address"<br><hr>
-	<i class="fas fa-phone-alt"></i>"Phone_Number"-<br> "Backup_Number"<br><hr>
-	    
+	<i class="fas fa-phone-alt"></i>"Phone_Number"-<br><hr> "Backup_Number"<br><hr>
 	</div>	
 </div>	
 EOT;
